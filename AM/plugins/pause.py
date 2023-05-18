b8d512ae9a2b425056b88c872d20a350bb575b6a
Sin-va-dev <132194355+Sin-va-dev@users.noreply.github.com>
2023-05-18 19:28:32 +0530

    Update pause.py

--- AM/plugins/pause.py
+++ AM/plugins/pause.py
@@ -26,7 +26,7 @@ async def iru_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await Anon.Irru_Ley_Poduvom_🙂(chat_id)
+    await Anon.Irru_Ley_Poduvom(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.first_name),
         reply_markup=close_keyboard
