4a0849027655470c75a8608074a6dc333abd9260
Sin-va-dev <132194355+Sin-va-dev@users.noreply.github.com>
2023-05-19 03:13:53 +0530

    Update userbot.py

--- AM/core/userbot.py
+++ AM/core/userbot.py
@@ -62,7 +62,7 @@ class Userbot(Client):
 
             try:
                 await self.one.send_message(
-                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ᴀssɪsᴛᴀɴᴛ ᴏɴᴇ sᴛᴀʀᴛᴇᴅ :**\n\n✨ ɪᴅ : `{self.one.id}`\n❄ ɴᴀᴍᴇ : {self.one.name}\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.one.username}"
+                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ᴀssɪsᴛᴀɴᴛ ᴏɴᴇ sᴛᴀʀᴛᴇᴅ :**\n\n✨ 𝔻𝔸𝕋𝔸 : `{self.one.id}`\n❄ ᴘʀᴏꜰɪʟᴇ : {self.one.name}\n💫 UsᴇʀNᴀᴍE : @{self.one.username}"
                 )
             except:
                 LOGGER(__name__).error(
