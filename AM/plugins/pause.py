c274f58bb9994cb21fbba6bb83720177dd5661f5
Sin-va-dev <132194355+Sin-va-dev@users.noreply.github.com>
2023-05-18 19:26:21 +0530

    Update pause.py

--- AM/plugins/pause.py
+++ AM/plugins/pause.py
@@ -20,13 +20,13 @@ PAUSE_COMMAND = get_command("PAUSE_COMMAND")
     & ~BANNED_USERS
 )
 @AdminRightsCheck
-async def pause_admin(cli, message: Message, _, chat_id):
+async def iru_admin(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await Anon.pause_stream(chat_id)
+    await Anon.Irru_Ley_Poduvom 🙂(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.first_name),
         reply_markup=close_keyboard
