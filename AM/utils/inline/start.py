3511aa7ea39cc696bf7b8c35ed18dccd3811a413
Sin-va-dev <132194355+Sin-va-dev@users.noreply.github.com>
2023-05-20 19:16:22 +0530

    Update start.py

--- AM/utils/inline/start.py
+++ AM/utils/inline/start.py
@@ -9,7 +9,7 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text=" נσιη мє ιη уσυя вαѕє ",
+                text="Jσιη мє ιη уσυя вαѕє",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
@@ -30,7 +30,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text=" נσιη мє ιη уσυя вαѕє ",
+                text="Jσιη мє ιη уσυя вαѕє",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
