2d08a04e3129ae5cd0308eb6b1c85448683d3cd7
Sin-va-dev <arnoldhanes1996@gmail.com>
2023-06-16 21:17:06 +0530

    Update block.py

--- AM/plugins/block.py
+++ AM/plugins/block.py
@@ -9,9 +9,9 @@ from AM.utils.database import add_gban_user, remove_gban_user
 from AM.utils.decorators.language import language
 
 # Command
-BLOCK_COMMAND = get_command("BLOCK_COMMAND")
-UNBLOCK_COMMAND = get_command("UNBLOCK_COMMAND")
-BLOCKED_COMMAND = get_command("BLOCKED_COMMAND")
+BLOCK_COMMAND = get_command(".block")
+UNBLOCK_COMMAND = get_command(".unblock")
+BLOCKED_COMMAND = get_command(".blocked")
 
 
 @app.on_message(filters.command(BLOCK_COMMAND) & SUDOERS)
